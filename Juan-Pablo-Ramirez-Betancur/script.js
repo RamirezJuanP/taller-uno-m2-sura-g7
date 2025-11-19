4509cfbdd3c85d2fa6e5c1d977ae27541beacc5e
Juan Pablo Ramirez <juanpablorb2000@gmail.com>
2025-11-19 10:10:20 -0500

    Resolviendo el segundo ejercicio

--- Juan-Pablo-Ramirez-Betancur/script.js
+++ Juan-Pablo-Ramirez-Betancur/script.js
@@ -17,3 +17,28 @@ compras.push("Leche", "Pan")
 compras.unshift("Huevos");
 
 console.log(compras)
+
+
+// EJERCICIO 2
+
+/*
+Ejercicio 2: Pila de Tareas Pendientes (LIFO)
+Inicias con la siguiente pila de tareas: let tareas = ["Revisar emails"];
+
+Tareas:
+
+Llega una tarea nueva: "Llamar al cliente". Ponla ENCIMA de la pila (al inicio).
+Llega otra tarea: "Preparar reporte". Ponla ENCIMA de la pila (al inicio).
+Completaste la última tarea que entró ("Preparar reporte"). Retírala de la pila.
+Completaste la siguiente tarea ("Llamar al cliente"). Retírala.
+*/
+
+let tareas = ["Revisar emails"];
+
+tareas.unshift("Llamar al cliente");
+tareas.unshift("Preparar reporte");
+
+tareas.shift();
+tareas.shift();
+
+console.log(tareas)
