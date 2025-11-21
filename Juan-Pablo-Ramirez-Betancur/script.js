045824683708e7543a3c9796e6ba64375a852ec7
Juan Pablo Ramirez <juanpablorb2000@gmail.com>
2025-11-21 08:16:59 -0500

    Resolviendo el septimo ejercicio

--- Juan-Pablo-Ramirez-Betancur/script.js
+++ Juan-Pablo-Ramirez-Betancur/script.js
@@ -84,3 +84,61 @@ colaImpresion.push("doc4.txt");
 colaImpresion.shift();
 
 console.log(colaImpresion);
+
+
+// EJERCICIO 5
+
+/*
+Ejercicio 5: Fila del Banco (Splice)
+Inicias con la siguiente fila: let fila = ["Ana", "Luis", "Carlos", "Maria"];
+
+Tareas:
+
+"Luis" (posición 1) se cansa y se va. Elimínalo.
+"Jorge" se cuela en la fila y se pone justo después de "Ana" (en la nueva posición 1). Insértalo sin eliminar a nadie.
+*/
+
+let fila = ["Ana", "Luis", "Carlos", "Maria"];
+
+fila.splice(1,1)
+fila.splice(1, 0, "Jorge");
+
+
+console.log(fila);
+
+
+// EJERCICIO 6
+
+/*
+Ejercicio 6: Mazo de Cartas
+Inicias con un pequeño mazo: let mazo = ["As", "Rey", "Reina", "Jota"];
+
+Tareas:
+
+Barajas el mazo invirtiendo el orden de las cartas.
+Sacas la carta de arriba (el primer elemento).
+*/
+
+let mazo = ["As", "Rey", "Reina", "Jota"];
+
+mazo.reverse();
+mazo.shift();
+
+
+console.log(mazo);
+
+
+// EJERCICIO 7
+
+/*
+Ejercicio 7: Gestión de Asientos de Cine
+Inicias con el estado de una fila de asientos: let asientos = ["Ocupado", "Libre", "Libre", "Libre", "Ocupado"];
+
+Tareas:
+
+Las tres personas en los asientos "Libre" (posiciones 1, 2 y 3) deben moverse. Cambia esos tres asientos a "Reservado". (Pista: fill puede usar un índice de inicio y fin).
+*/
+
+let asientos = ["Ocupado", "Libre", "Libre", "Libre", "Ocupado"];
+
+
